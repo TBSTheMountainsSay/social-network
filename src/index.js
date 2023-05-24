9a45eebc5e693149f0d7d9df2b5b8d1751f50123
TBSTheMountainsSay <blackswadow@yandex.ru>
2023-05-24 18:12:36 +0400

    Changed Router

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import './index.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './Redux/redux-store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
